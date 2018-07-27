0f232e8b1ff81d4cd396ee95d601ea6edf1cb9d6
taohumyai <31985816+taohumyai@users.noreply.github.com>
2018-07-27 14:09:47 +0700

    555

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("body").queryLoader2({
+        barColor: "#fff",
+        backgroundColor: "#F78181",
+        percentage: true,
+        barHeight: 20,
+        completeAnimation: "grow",
+        minimumTime: 5000
+    });
+});
